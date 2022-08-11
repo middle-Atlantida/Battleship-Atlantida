4d85d53ea59ffe38b029a0cc22b8c87a717e2bb3
ma-havaev <ma-havaev@yandex-team.ru>
2022-08-11 17:55:39 +0300

    fix jsx elem to reactnode, add static error page

--- src/pages/ServerError/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { ErrorLayout } from 'components/ErrorLayout';
-
-export const ServerError = () => (
-    <ErrorLayout
-        title="500"
-        text={'Внутренняя ошибка сервера.\nМы о ней знаем и скоро ее исправим!'}
-    />
-);
