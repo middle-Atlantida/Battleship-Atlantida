1caf2adb4b506a16f312215d6444eeeaccea8948
Violet <eerfeer@yandex.ru>
2022-08-14 19:54:33 +0300

    Fix lockfile

--- src/pages/Root/index.tsx
+++ src/pages/Root/index.tsx
@@ -1,8 +1,5 @@
 import { SignIn } from 'pages/SignIn';
-import { SignUp } from 'pages/SignUp';
-
 import React from 'react';
-import { MainMenu } from '../MainMenu';
 
 export const Root = () => (
     <>
