ca774ddfaec2a74b3512f92fb393d9cc3946cae4
Oleg Kozlov <fixyarik@mail.ru>
2022-08-14 12:53:05 +0300

    Update src/pages/MainPage/index.tsx
    Co-authored-by: Evgeniy Gorodilov <gorodilov.box@gmail.com>

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -3,7 +3,7 @@ import cn from 'classnames';
 import css from './MainPage.module.css';
 
 const MainPage = () => (
-    <div className={cn(css.container)}>
+    <div className={css.container}>
         <h1 className={cn(css.mainPageTitle)}>МОРСКОЙ БОЙ</h1>
         <ul className={cn(css.menuSelection)}>
             <li>Играть</li>
