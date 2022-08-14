b6c7ffe614790df0d9980a5d1eddbff40a9cffad
Oleg Kozlov <fixyarik@mail.ru>
2022-08-14 12:59:30 +0300

    Feature/4 main page (#4)
    * pull main
    
    * added MainPage component && added script fix:eslint
    
    * Update src/pages/MainPage/index.tsx
    
    Co-authored-by: Evgeniy Gorodilov <gorodilov.box@gmail.com>
    
    Co-authored-by: kozlov-oe <O.Kozlov@otpbank.ru>
    Co-authored-by: Evgeniy Gorodilov <gorodilov.box@gmail.com>

--- src/pages/Root/index.tsx
+++ src/pages/Root/index.tsx
@@ -1,9 +1,10 @@
 import { SignIn } from 'pages/SignIn';
 
 import React from 'react';
+import MainPage from '../MainPage';
 
 export const Root = () => (
     <>
-        <SignIn/>
+        <MainPage/>
     </>
 );
