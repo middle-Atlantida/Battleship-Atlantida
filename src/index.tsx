170b8b9bd165568bd090260223478abb425afb57
ma-havaev <ma-havaev@yandex-team.ru>
2022-08-29 22:35:46 +0300

    ADD: service-worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { createRoot } from 'react-dom/client';
 import { Root } from 'pages/Root';
 import { theme } from 'utils/theme';
@@ -6,28 +7,28 @@ import { StyledEngineProvider, ThemeProvider } from '@mui/material';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { ErrorBoundary } from 'components/ErrorBoundry';
-import { Provider } from 'react-redux';
 import { configureStore } from 'store/store';
+import { startServiceWorker } from './serviceWorker';
 
 const store = configureStore();
 
 const App = () => (
     <React.StrictMode>
-        <BrowserRouter>
-            <ErrorBoundary>
-                <StyledEngineProvider injectFirst>
-                    <ThemeProvider theme={theme}>
-                        <Root/>
-                    </ThemeProvider>
-                </StyledEngineProvider>
-            </ErrorBoundary>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <ErrorBoundary>
+                    <StyledEngineProvider injectFirst>
+                        <ThemeProvider theme={theme}>
+                            <Root/>
+                        </ThemeProvider>
+                    </StyledEngineProvider>
+                </ErrorBoundary>
+            </BrowserRouter>
+        </Provider>
     </React.StrictMode>
 );
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-createRoot(document.getElementById('root')!).render(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
-);
+createRoot(document.getElementById('root')!).render(<App/>);
+
+startServiceWorker();
