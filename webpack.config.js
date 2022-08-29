1104071198bfbf06c57507d01646b4c6264c0054
ma-havaev <ma-havaev@yandex-team.ru>
2022-08-29 22:35:46 +0300

    ADD: service-worker

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.tsx',
+    entry: {
+        main: './src/index.tsx',
+        'service-worker': './public/service-worker.js',
+    },
     devtool: 'inline-source-map',
     devServer: {
         static: {
