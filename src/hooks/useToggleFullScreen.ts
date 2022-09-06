41c538de218a997dd60b632706f474471b3ebc6a
ma-havaev <ma-havaev@yandex-team.ru>
2022-09-06 16:08:08 +0300

    fix for pr comments

--- src/hooks/useToggleFullScreen.ts
+++ src/hooks/useToggleFullScreen.ts
@@ -1,13 +1,15 @@
 import { useEffect } from 'react';
 
+import { KEY_CODES } from 'const/keyCodes';
+
 export const useToggleFullScreen = () => {
     useEffect(() => {
         const toggleFullScreen = (event: KeyboardEvent) => {
-            if (event.code !== 'Space' && event.code !== 'Escape') return;
+            if (event.code !== KEY_CODES.f && event.code !== KEY_CODES.esc) return;
 
-            if (event.code === 'Space' && !document.fullscreenElement) {
+            if (event.code === KEY_CODES.f && !document.fullscreenElement) {
                 document.documentElement.requestFullscreen();
-            } else if (event.code === 'Escape' && document.exitFullscreen) {
+            } else if (event.code === KEY_CODES.esc && document.exitFullscreen) {
                 document.exitFullscreen();
             }
         };
