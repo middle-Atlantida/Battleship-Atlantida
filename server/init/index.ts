70e708bb2d7595808f713fc9eabc3b24e9bdb4a9
kozlov-oe <O.Kozlov@otpbank.ru>
2022-10-09 13:44:51 +0300

    added models for forum

--- server/init/index.ts
+++ server/init/index.ts
@@ -1,10 +1,13 @@
 import {Sequelize} from "sequelize-typescript";
 import {sequelizeOptions} from "../configs/db.config";
+import {forumModel} from "../models/forum";
+import {forumCommentModel} from "../models/forumComment";
 
 export const sequelize = new Sequelize(sequelizeOptions);
 
 // Инициализируем здесь модели
-// export const User = sequelize.define('User', userModel, {});
+export const forumName = sequelize.define('ForumName', forumModel, {});
+export const forumComment = sequelize.define('ForumComment', forumCommentModel, {});
 
 export async function dbConnect() {
     try {
