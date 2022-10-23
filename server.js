d8b83b8468557e313a9588fc6c700a3785147b89
Oleg Kozlov <fixyarik@mail.ru>
2022-10-23 17:42:43 +0300

    Feature/30 add models (#27)
    * added docker-compose && srquelize
    
    * fixes cinfig
    
    * fixes settings
    
    * fixes conflicts
    
    * added models for forum
    
    Co-authored-by: kozlov-oe <O.Kozlov@otpbank.ru>

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(`${__dirname}/build/`));
+const { Client } = require('pg');
+const { dbConnect } = require("./server");
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(`${__dirname}/build/index.html`));
+const client = new Client({
+    host: 'localhost',
+    database: 'battleship-db',
+    user: 'postgres',
+    password: 'postgres',
+    port: 5432,
 });
 
-app.listen(PORT, () => {
-    // eslint-disable-next-line no-console
-    console.log(`App start on http://localhost:${PORT}`);
+client.connect().then(() => {
+    try {
+        dbConnect().then();
+        app.use(express.static(`${__dirname}/build/`));
+
+        app.get('/*', (req, res) => {
+            res.sendFile(path.join(`${__dirname}/build/index.html`));
+        });
+
+        app.listen(PORT, () => {
+            // eslint-disable-next-line no-console
+            console.log(`App start on http://localhost:${PORT}`);
+        });
+    } catch (err) {
+        console.log('connect', err);
+    }
 });
